3fc6b4672a0942126b69a6b16e36be62477dad17
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-19 08:25:58 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+// data.js
+const wordData = [
+  { entry: { id: 1, form: "neko" } },
+  { entry: { id: 2, form: "inu" } },
+  { entry: { id: 3, form: "tori" } },
+  // ... 省略
+  { entry: { id: 200, form: "hana" } }
+];
